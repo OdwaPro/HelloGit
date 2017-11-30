062ea816fdcb2bebc90fd8eaff6399093e9a4504
Odwa <34059661+OdwaPro@users.noreply.github.com>
2017-11-30 12:55:19 +0200

    Update Gitt.java

--- HeeloGit/src/git/Gitt.java
+++ HeeloGit/src/git/Gitt.java
@@ -5,6 +5,7 @@ public class Gitt {
 	public static void main(String[]args){
 		System.out.print("Blahhh Blahhh Blahhh");
 		System.out.print("Blahhh Blahhh Blahhh");
+		System.out.print("Horrrayyy");
 	}
 
 }
